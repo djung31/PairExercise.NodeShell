74e286b87074b16a8b6f598578b6be901a0d6698
David Jung <djung31@gmail.com>
2018-09-18 11:25:23 -0500

    implemented cat command

--- bash.js
+++ bash.js
@@ -1,17 +1,23 @@
-//Outpiut a prompt
+// Import commands from other JS
 const pwd = require('./pwd');
 const ls = require('./ls');
+const cat = require('./cat');
+
+//Output a prompt
 process.stdout.write('prompt > ');
 
 //stdin 'data' event fires after a user types in a line
 
 process.stdin.on('data', data => {
   const cmd = data.toString().trim(); //removed the newline
+  let splitCmd = cmd.split(' '); // splits cmd into an array of words
 
   if (cmd === 'pwd') {
     pwd();
   } else if (cmd === 'ls') {
     ls();
+  } else if (splitCmd[0] === 'cat') {
+    cat(splitCmd[1]);
   }
   //process.stdout.write('You typed: ' + cmd);
   //process.stdout.write('prompt > ');
